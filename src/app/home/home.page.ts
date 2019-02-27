c26113baafd1f78257eb3ff59da5174e0950d2c7
hugonh <hugobiller@hotmail.com>
2019-02-26 22:27:02 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Aluno } from "../modelos/Alunos";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public alunos: Aluno[];
 
+  constructor(public http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get<Aluno[]>('http://gilsonpolito-api.herokuapp.com/alunos')
+      .subscribe((alunos) => { this.alunos = alunos })
+  }
 }
